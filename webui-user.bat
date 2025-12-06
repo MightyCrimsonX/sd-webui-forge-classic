ec1c83f8a35848e8a56a566eac5fbbdb836cb147
MightyCrimsonX <mightycrimson0908@gmail.com>
2025-12-06 16:50:23 -0500

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -5,6 +5,7 @@
 :: set VENV_DIR=
 
 set COMMANDLINE_ARGS= --no-download-sd-model
+set TORCH_COMMAND=pip install torch==2.1.2 torchvision==0.16.2 --extra-index-url https://download.pytorch.org/whl/cu121
 
 :: --xformers --sage --uv
 :: --pin-shared-memory --cuda-malloc --cuda-stream
